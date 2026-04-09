6e202b4ca5e157c404c884a2060c3e4dd30f466d
Cgarc657 <carlos.garcia529440.cg@gmail.com>
2026-04-09 01:22:29 -0500

    initial coinhub layout

---
+++ src/index.js
@@ -0,0 +1,35 @@
+import { Hono } from "hono";
+import coins from "./routes/coins.js";
+import { isApiError } from "./utils/errors.js";
+import { sendError } from "./utils/response.js";
+
+const app = new Hono();
+const api = new Hono();
+
+app.use("*", async (c, next) => {
+  c.set("traceId", crypto.randomUUID());
+  await next();
+});
+
+api.route("/coins", coins);
+
+app.route("/api", api);
+
+app.notFound((c) => {
+  return sendError(c, 404, "NOT_FOUND", "Route not found.");
+});
+
+app.onError((error, c) => {
+  if (isApiError(error)) {
+    return sendError(c, error.status, error.code, error.message, error.details);
+  }
+
+  return sendError(
+    c,
+    500,
+    "INTERNAL_SERVER_ERROR",
+    "An unexpected server error occurred.",
+  );
+});
+
+export default app;
